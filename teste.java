35ec0454831dcea0da5d9dfa3d3ccd4938e9cfe3
thais <tha_cambi@hotmail.com>
2013-01-28 22:52:35 -0200

    alterando teste java

--- teste.java
+++ teste.java
@@ -2,6 +2,7 @@ class teste{
 public static void main (String[] args){
   System.out.println("Exemplo de teste de uso git");
   System.out.println("Aula de git");
+ System.out.println("Mais um teste de git");
 
 }
 }
